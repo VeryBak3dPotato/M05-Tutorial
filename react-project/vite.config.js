68b6689db2c35d43ef56a8a52e1cdd34ef47891a
Connor Bailey <cebailey92@gmail.com>
2025-02-17 13:10:12 -0500

    trying again

--- react-project/vite.config.js
+++ react-project/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/VeryBak3dPotato/M05-Tutorial/react-project",
+  base: "/react-project/",
   plugins: [react()],
 });
